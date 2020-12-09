afcccd67f5867e400f673f92e29bd697cd814f17
riadh <gharbiriadh16@gmail.com>
2020-12-09 17:01:48 +0100

    popup secur
    Co-Authored-By: riadhrahma <11444795+riadhrahma@users.noreply.github.com>

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -11,9 +11,9 @@ const RESOURCES = {
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "308141cf70404cf15033457298ddf18a",
-"/": "308141cf70404cf15033457298ddf18a",
-"main.dart.js": "59add0b1171bf9ece56d0a49c230a3b5",
+"index.html": "cf6e1cf58b9f88e4b35cc8092a20bced",
+"/": "cf6e1cf58b9f88e4b35cc8092a20bced",
+"main.dart.js": "dc1fad1c2d620ffa7d6233a0b8745841",
 "manifest.json": "2fa0ab7ce7ab5444e27bcdb28c76e3fe",
 "version.json": "c2c3e981e7378aa205950ea9b545caa9"
 };
